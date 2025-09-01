1c7bf27fccacc797fd3b209ee97110a0055fab0b
kb-pk <209384531+kb-pk@users.noreply.github.com>
2025-09-02 00:02:43 +0200

    Removed "all" output mode

--- scheduler/ProgramState.py
+++ scheduler/ProgramState.py
@@ -18,23 +18,9 @@ class ProgramState:
 
             self.__current_state = self.State(value)
 
-    class __OutputState:
-        class State(Enum):
-            makespan = 0
-            energy = 1
-            all = 2
-
-        def __init__(self):
-            self.__current_state = self.State.makespan
-
-        def get(self):
-            return self.__current_state
-
-        def set(self, value: State):
-            if not self.State.__contains__(value):
-                raise ValueError("Invalid enum value")
-
-            self.__current_state = self.State(value)
+    # after deleting "all" it's the same class...
+    class __OutputState(__SchedulingState):
+        pass
 
     class __UserInterfaceState:
         class State(Enum):
