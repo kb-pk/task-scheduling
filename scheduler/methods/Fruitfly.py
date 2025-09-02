591fb2a5686704995ecab007461d900230bca816
kb-pk <209384531+kb-pk@users.noreply.github.com>
2025-09-02 17:19:08 +0200

    Refactor - Fruitfly

--- scheduler/methods/Fruitfly.py
+++ scheduler/methods/Fruitfly.py
@@ -1,78 +1,68 @@
+from platform import machine
+
 import numpy as np
 import scheduler.Common as Common
+from lang.Lang import T
+from scheduler.Logger import Logger
+from scheduler.MethodCache import MethodCache
+from scheduler.ProgramState import ProgramState
+from scheduler.Registry import MethodRegistrator
 from scheduler.methods.BaseMethod import BaseMethod
-from scheduler.Parameters import ParamDef
+from scheduler.Parameters import ParamDef, ParamDef2, ParamValueTypes
+from scheduler.methods.ParticleSwarmMethod import ParticleSwarmMethod
 
-class FruitflyMethod(BaseMethod):
-    PARAM_DEFS = [
-        ParamDef("iterations", "int", 100, "Number of iterations", min_value=1),
-        ParamDef("population_size", "int", 30, "Population size", min_value=2),
-        ParamDef("vision_step", "float", 5.0, "Vision phase standard deviation", min_value=0.0, max_value=100.0),
-        ParamDef("show_chart", "bool", True, "Display Gantt chart after run")
-    ]
+@MethodRegistrator.register_class
+class FruitflyMethod(ParticleSwarmMethod):
+    def __init__(self, state: ProgramState, logger: Logger, t: T, cache: MethodCache):
+        super().__init__(state, logger, t, cache)
 
-    def __init__(self,
-                 iterations=100,
-                 population_size=30,
-                 vision_step=5.0,
-                 show_chart=True):
-        super().__init__(iterations=iterations, show_chart=show_chart)
-        self.population_size = population_size
-        self.vision_step = vision_step
-        self.population = None
-        self.best_pos = None
-        self.best_score = None
-        self.other_score = None
+        params = [
+            ParamDef2(self.T.t("step size"), ParamValueTypes.FLOAT, 10, self.T.t("Step size in vision phase"),
+                      min_value=0.0, max_value=len(self.machines))
+        ]
 
-    def set_parameters(self, iterations=100, population_size=30, vision_step=5.0, show_chart=True):
-        self.iterations = iterations
-        self.population_size = population_size
-        self.vision_step = vision_step
-        self.show_chart = show_chart
+        self.PARAM_DEFS += params
 
-    def get_name(self):
-        return "fruitfly"
+        self._vision_step = params[0].get_value()
 
-    # --- lifecycle ---
-    def initialize(self):
-        tasks_num = len(self.tasks)
-        machines_num = len(self.machines)
-        self.population = np.random.uniform(0, machines_num - 1, size=(self.population_size, tasks_num))
-        fitness = np.array([Common.vector_fitness(x, self.etc, self.machines, Common.scheduling_mode)[0] for x in self.population])
-        idx = np.argmin(fitness)
-        self.best_pos = self.population[idx].copy()
-        self.best_score, self.other_score = Common.vector_fitness(self.best_pos, self.etc, self.machines, Common.scheduling_mode)
+        self.name = "Fruitfly"
+        self.description = self.T.td({
+            self.state.lang.State.pl_PL: """
+            Algorytm optymalizacyjny Fruitfly. Rodzaj algorytmu optymalizacyjnego particle swarm.
+            
+            Osobniki i przestrzeń poszukiwań konstruowane są tak samo, jak zostało przedstawione w opisie metody Dragonfly.
+            
+            Algorytm definiuje 2 fazy:
+            1. Szukanie zapachu (smell search), czyli znalezienie pozycji osobnika z najlepszą wartością funkcji przystosowania, 
+            2. Przemieszczenie się w stronę zapachu (vision search).
+            
+            Algorytm wykonuje te 2 fazy w pętli aż do osiągnięcia warunku końcowego.
+            """,
+            self.state.lang.State.en_GB: """
+            Fruitfly optimisation algorithm. A type of particle swarm optimisation algorithm.
+            
+            Entities and the search space are constructed in the same way as is defined in the description of the Dragonfly method.
+            
+            The algorithm defines 2 phases:
+            1. Smell search -  finding the position of the entity with the best value of the fitness function,
+            2. Vision search - "flying" toward the position of the smell.
+            
+            The algorithm performs these 2 phases in a loop until a stop condition is reached.
+            """
+        })
 
     def optimize(self):
-        tasks_num = len(self.tasks)
-        machines_num = len(self.machines)
-        for it in range(self.iterations):
-            # Smell search
-            smell_vals = np.array([Common.vector_fitness(x, self.etc, self.machines, Common.scheduling_mode)[0] for x in self.population])
-            i_smell = np.argmin(smell_vals)
-            X_smell = self.population[i_smell].copy()
+        while not self.stop():
+            # smell search
+            best_smell = min(
+                self.population,
+                key=lambda x: self._fitness(self.build_schedule_map(x)).scheduling()
+            )
 
-            # Vision search
-            for i in range(self.population_size):
-                self.population[i] = X_smell + np.random.randn(tasks_num) * self.vision_step
-                self.population[i] = np.clip(self.population[i], 0, machines_num - 1)
-                main_val, other_val = Common.vector_fitness(self.population[i], self.etc, self.machines, Common.scheduling_mode)
-                if main_val < self.best_score:
-                    self.best_score = main_val
-                    self.other_score = other_val
-                    self.best_pos = self.population[i].copy()
-                    print(f"[{it}] new best {Common.scheduling_modes[Common.scheduling_mode]}: {self.best_score:.4f}")
+            # vision search
+            for inx, individual in enumerate(self.population):
+                self.population[inx] = best_smell + np.random.randn(len(self.tasks)) * self._vision_step
+                self.population[inx] = np.clip(self.population[inx], 0, len(self.machines) - 1)
 
-    def get_best_solution(self):
-        return self.best_pos
-
-    def build_schedule_map(self, position_vector):
-        assign = Common.decode_position_vector(position_vector, len(self.machines))
-        schedule_map = {m: [] for m in self.machines.index.values}
-        for task_id, m_id in enumerate(assign):
-            schedule_map[int(m_id)].append(task_id)
-        return schedule_map
-
-if __name__ == "__main__":
-    alg = FruitflyMethod(iterations=100, population_size=30, vision_step=5.0, show_chart=True)
-    alg.run()
+            self._evaluate_population_update_best()
+            self._epoch += 1
