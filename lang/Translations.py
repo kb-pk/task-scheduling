446b8e3b4dc390291f957067bd2f68452c41f01a
kb-pk <209384531+kb-pk@users.noreply.github.com>
2025-09-02 12:33:20 +0200

    Added some translation strings

--- lang/Translations.py
+++ lang/Translations.py
@@ -17,11 +17,16 @@ class Translations:
 
                 "Switch scheduling mode (current mode: ": "Zmień tryb szeregowania (obecnie: ",
                 "Switch output mode (current mode: ": "Zmień tryb wyświetlania (obecnie: ",
+                "Switch stop criterion (current mode: ": "Zmień warunek stopu (obecnie: ",
                 "Exit program": "Wyjdź z programu",
                 "Invalid choice": "Nieprawidłowy wybór",
 
+                "Stop criterion": "Warunek stopu",
+                "Criterion for stopping the evolution": "Warunek zakończenia ewolucji",
                 "Iterations": "Iteracje",
                 "Number of iterations (epochs)": "Liczba iteracji (epok)",
+                "Fitness function value": "Wartość funkcji przystosowania",
+                "The value which the algorithm is optimising (": "Wartość, którą optymalizuje algorytm (",
                 "Population size": "Ilość osobników",
                 "Must be even": "Musi być parzysta",
                 "Crossover points": "Punkty krzyżowania",
@@ -29,6 +34,8 @@ class Translations:
                 "Mutation probability": "Prawdopodobieństwo mutacji",
                 "Gene mutation probability": "Prawdopodobieństwo mutacji genu",
 
+                "Number of tasks exceeds number of machines - machines cannot be without tasks":
+                    "Liczba zadań przewyższa liczbę maszyn - maszyny nie mogą być bez zadań",
             }
         }
 
