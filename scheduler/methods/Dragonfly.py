d8a446275deca8d77fa7c5f38890f9ea51d7e646
kb-pk <209384531+kb-pk@users.noreply.github.com>
2025-09-02 16:57:42 +0200

    Refactor - Dragonfly

--- scheduler/methods/Dragonfly.py
+++ scheduler/methods/Dragonfly.py
@@ -1,139 +1,150 @@
+from abc import ABC
+
 import numpy as np
 import scheduler.Common as Common
+from lang.Lang import T
+from scheduler.Logger import Logger
+from scheduler.MethodCache import MethodCache
+from scheduler.ProgramState import ProgramState
+from scheduler.Registry import MethodRegistrator
 from scheduler.methods.BaseMethod import BaseMethod
-from scheduler.Parameters import ParamDef
+from scheduler.Parameters import ParamDef, ParamDef2, ParamValueTypes
+from scheduler.methods.ParticleSwarmMethod import ParticleSwarmMethod
 
-class DragonflyMethod(BaseMethod):
-    PARAM_DEFS = [
-        ParamDef("iterations", "int", 100, "Number of iterations", min_value=1),
-        ParamDef("population_size", "int", 30, "Swarm size", min_value=2),
-        ParamDef("w_inertia", "float", 0.9, "Inertia weight", min_value=0.0, max_value=5.0),
-        ParamDef("w_separation", "float", 0.1, "Separation weight", min_value=0.0, max_value=5.0),
-        ParamDef("w_alignment", "float", 0.1, "Alignment weight", min_value=0.0, max_value=5.0),
-        ParamDef("w_cohesion", "float", 0.1, "Cohesion weight", min_value=0.0, max_value=5.0),
-        ParamDef("w_food", "float", 2.0, "Food attraction weight", min_value=0.0, max_value=10.0),
-        ParamDef("w_enemy", "float", 1.0, "Enemy repulsion weight", min_value=0.0, max_value=10.0),
-        ParamDef("neighbour_radius_factor", "float", 0.5, "Neighbour radius factor", min_value=0.0, max_value=10.0),
-        ParamDef("show_chart", "bool", True, "Display Gantt chart after run")
-    ]
+@MethodRegistrator.register_class
+class DragonflyMethod(ParticleSwarmMethod, ABC):
+    def __init__(self, state: ProgramState, logger: Logger, t: T, cache: MethodCache):
+        super().__init__(state, logger, t, cache)
 
-    def __init__(self,
-                 iterations=100,
-                 population_size=30,
-                 w_inertia=0.9,
-                 w_separation=0.1,
-                 w_alignment=0.1,
-                 w_cohesion=0.1,
-                 w_food=2.0,
-                 w_enemy=1.0,
-                 neighbour_radius_factor=0.5,
-                 show_chart=True):
-        super().__init__(iterations=iterations, show_chart=show_chart)
-        self.population_size = population_size
-        self.w_inertia = w_inertia
-        self.w_separation = w_separation
-        self.w_alignment = w_alignment
-        self.w_cohesion = w_cohesion
-        self.w_food = w_food
-        self.w_enemy = w_enemy
-        self.neighbour_radius_thresh = neighbour_radius_factor * (len(self.machines) - 1)
+        params = [
+            ParamDef2(self.T.t("Inertia"), ParamValueTypes.FLOAT, 0.9, self.T.t("Movement inertia"),
+                      min_value=0.0, max_value=1.0),
+            ParamDef2(self.T.t("Separation"), ParamValueTypes.FLOAT, 0.1, self.T.t("Separation between entities"),
+                      min_value=0.0, max_value=1.0),
+            ParamDef2(self.T.t("Alignment"), ParamValueTypes.FLOAT, 0.1,
+                      self.T.t("How closely entity's speed matches that of other entities"),
+                      min_value=0.0, max_value=1.0),
+            ParamDef2(self.T.t("Cohesion"), ParamValueTypes.FLOAT, 0.1,
+                      self.T.t("How much the entity is drawn to the center of their neighbourhood"),
+                      min_value=0.0, max_value=1.0),
+            ParamDef2(self.T.t("Food attraction"), ParamValueTypes.FLOAT, 2.0,
+                      self.T.t("How much the entity is drawn to food sources"),
+                      min_value=0.0, max_value=10.0),
+            ParamDef2(self.T.t("Enemy repulsion"), ParamValueTypes.FLOAT, 1.0,
+                      self.T.t("How much the entity is drawn away from enemy sources"),
+                      min_value=0.0, max_value=10.0),
+            ParamDef2(self.T.t("Neighbour radius"), ParamValueTypes.FLOAT, (len(self.machines) - 1) / 2,
+                      "",
+                      min_value=0.0, max_value=len(self.machines)),
+        ]
 
-        self.X = None  # positions
-        self.V = None  # velocities
-        self.best_pos = None
-        self.worst_pos = None
-        self.best_score = None
-        self.other_score = None
+        self.PARAM_DEFS += params
 
-    def set_parameters(self, iterations=100, population_size=30, w_inertia=0.9, w_separation=0.1,
-                       w_alignment=0.1, w_cohesion=0.1, w_food=2.0, w_enemy=1.0,
-                       neighbour_radius_factor=0.5, show_chart=True):
-        self.iterations = iterations
-        self.population_size = population_size
-        self.w_inertia = w_inertia
-        self.w_separation = w_separation
-        self.w_alignment = w_alignment
-        self.w_cohesion = w_cohesion
-        self.w_food = w_food
-        self.w_enemy = w_enemy
-        self.neighbour_radius_factor = neighbour_radius_factor
-        self.neighbour_radius_thresh = neighbour_radius_factor * (len(self.machines) - 1)
-        self.show_chart = show_chart
+        self._inertia = params[0].get_value()
+        self._separation = params[1].get_value()
+        self._alignment = params[2].get_value()
+        self._cohesion = params[3].get_value()
+        self._food_attraction = params[4].get_value()
+        self._enemy_repulsion = params[5].get_value()
+        self._neighbour_radius = params[6].get_value()
 
-    def get_name(self):
-        return "dragonfly"
+        self.name = "Dragonfly"
+        self.description = self.T.td({
+            self.state.lang.State.pl_PL: """
+            Algorytm optymalizacyjny Dragonfly. Rodzaj algorytmu optymalizacyjnego particle swarm.
+    
+            Algorytm polega na zdefiniowaniu w przestrzeni poszukiwań miejsc z jedzeniem (najlepszym rozwiązaniem z poprzedniej epoki) i z wrogami (najgorszym rozwiązaniem z poprzedniej epoki).
+            Poziom przyciągania do źródeł jedzenia i odpychania od wrogów jest dyktowany parametrami.
+    
+            Osobniki w pobliżu (próg określony parametrem) tworzą sąsiedztwo danego osobnika.
+    
+            Dodatkowo zdefiniowane są parametry (wagi) w postaci:
+            1. Bezwładności osobników (inertia)
+            2. Separacji osobników od siebie (separation),
+            3. Poruszania się z podobną prędkością co reszta osobników (alignment),
+            4. Poruszania się w stronę centrum swojego sąsiedztwa (cohesion).
+    
+            Przestrzeń poszukiwań można sobie wyobrazić jako N-wymiarową (M = liczba zadań) przestrzeń z osobnikami "latającymi" wewnątrz niej zgodnie z ustalonymi parametrami.
+            Pozycja (koordynaty) osobników wyrażają harmonogram w postaci [machine_id, machine_id, ...], gdzie indeks to numer zadania wykonywanego przez maszynę.
+    
+            Przykładowo, dla 3 zadań osobniki "latają" po 3-wymiarowej przestrzeni, gdzie ich koordynaty to harmonogram wszystkich maszyn.
+            """,
 
-    # --- lifecycle ---
+            self.state.lang.State.en_GB: """
+            Dragonfly optimisation algorithm. A type of particle swarm optimisation algorithm.
+    
+            Algorithm defines places with food (best solution from last epoch) and enemies (worst solution from last epoch) in the search space.
+            Levels of attraction toward food sources and repulsion from enemies are defined with parameters.
+    
+            Entities close (threshold is defined with a parameter) to the specific entity are considered its neighbourhood.
+    
+            Additionally, the following parameters (weights) are defined:
+            1. Inertia of entities,
+            2. Separation of entities from each other,
+            3. Alignment of the entity in terms of speed with the rest of entities,
+            4. Cohesion of entities in terms of being drawn to the center of their neighbourhood.
+    
+            The search space can be visualised as a N-dimensional (N = task number) space with the entities "flying" inside of it according to the defined parameters.
+            Entity's position (its coordinates) represent a schedule in the form of [machine_id, machine_id, ...], where the index denotes the task number assigned to the machine.
+    
+            For example, for 3 machines the entities "fly" through a 3-dimensional space, where each entity's coordinates are a schedule for all the machines.
+            """
+        })
+
+    def _generate_population(self):
+        self.population = np.random.uniform(0, len(self.machines) - 1, size=(self._pop_size, len(self.tasks)))
+        self._velocity = np.zeros_like(self.population)
+
+    def __set_food_and_enemies(self):
+        fitness_of_individuals = [
+             self._fitness(self.build_schedule_map(individual)).scheduling() for individual in self.population
+        ]
+
+        food_inx = np.argmin(fitness_of_individuals)
+        enemies_inx = np.argmax(fitness_of_individuals)
+
+        self._food = self.population[food_inx]
+        self._enemies = self.population[enemies_inx]
+
     def initialize(self):
-        tasks_num = len(self.tasks)
-        machines_num = len(self.machines)
-        self.X = np.random.uniform(0, machines_num - 1, size=(self.population_size, tasks_num))
-        self.V = np.zeros_like(self.X)
-        fitness = np.array([Common.vector_fitness(x, self.etc, self.machines, Common.scheduling_mode)[0] for x in self.X])
-        b_idx = np.argmin(fitness)
-        w_idx = np.argmax(fitness)
-        self.best_pos = self.X[b_idx].copy()
-        self.worst_pos = self.X[w_idx].copy()
-        self.best_score, self.other_score = Common.vector_fitness(self.best_pos, self.etc, self.machines, Common.scheduling_mode)
+        super().initialize()
+
+    def _evaluate_population(self):
+        super()._evaluate_population()
+
+        self.__set_food_and_enemies()
 
     def optimize(self):
-        for it in range(self.iterations):
-            self._iterate()
-            cur, oth = Common.vector_fitness(self.best_pos, self.etc, self.machines, Common.scheduling_mode)
-            if cur < self.best_score:
-                self.best_score = cur
-                self.other_score = oth
-                print(f"[{it}] new best {Common.scheduling_modes[Common.scheduling_mode]}: {self.best_score:.4f}")
+        while not self.stop():
+            dist = np.linalg.norm(self.population[:, None, :] - self.population[None, :, :], axis=2)
 
-    def get_best_solution(self):
-        return self.best_pos
+            for i in range(self._pop_size):
+                neighbors = np.where((dist[i] < self._neighbour_radius) & (dist[i] > 0))[0]
 
-    def build_schedule_map(self, position_vector):
-        assign = Common.decode_position_vector(position_vector, len(self.machines))
-        schedule_map = {m: [] for m in self.machines.index.values}
-        for task_id, m_id in enumerate(assign):
-            schedule_map[int(m_id)].append(task_id)
-        return schedule_map
+                if len(neighbors) > 0:
+                    # Separation
+                    S = -np.sum(self.population[neighbors] - self.population[i], axis=0)
+                    # Alignment
+                    A = np.mean(self._velocity[neighbors], axis=0)
+                    # Cohesion
+                    C = np.sum(np.mean(self.population[neighbors], axis=0) - self.population[i], axis=0)
+                    # Attraction to food (best solution)
+                    F = self._food - self.population[i]
+                    # Distraction from enemy (worst solution)
+                    Rv = self._enemies + self.population[i]
+                    # Velocity and position update
+                    self._velocity[i] = (self._inertia * self._velocity[i] +
+                                         self._separation * S +
+                                         self._alignment * A +
+                                         self._cohesion * C +
+                                         self._food_attraction * F +
+                                         self._enemy_repulsion * Rv)
+                    self.population[i] += self._velocity[i]
+                else:
+                    # Random walk (Levy-like)
+                    self.population[i] += np.random.randn(len(self.tasks)) * (self.population[i] - self._enemies)
+                # Enforce bounds
+                self.population[i] = np.clip(self.population[i], 0, len(self.machines) - 1)
 
-    # --- core iteration ---
-    def _iterate(self):
-        tasks_num = len(self.tasks)
-        machines_num = len(self.machines)
-        dist = np.linalg.norm(self.X[:, None, :] - self.X[None, :, :], axis=2)
-        prev_best_val = Common.vector_fitness(self.best_pos, self.etc, self.machines, Common.scheduling_mode)[0]
-        prev_worst_val = Common.vector_fitness(self.worst_pos, self.etc, self.machines, Common.scheduling_mode)[0]
-
-        for i in range(self.population_size):
-            neighbors = np.where((dist[i] < self.neighbour_radius_thresh) & (dist[i] > 0))[0]
-            if len(neighbors) > 0:
-                S = -np.sum(self.X[neighbors] - self.X[i], axis=0)               # separation
-                A = np.mean(self.V[neighbors], axis=0)                            # alignment
-                C = np.mean(self.X[neighbors], axis=0) - self.X[i]                # cohesion
-                F = self.best_pos - self.X[i]                                     # attraction to food
-                Rv = self.worst_pos + self.X[i]                                   # repulsion (as in original)
-                self.V[i] = (
-                    self.w_inertia * self.V[i] +
-                    self.w_separation * S +
-                    self.w_alignment * A +
-                    self.w_cohesion * C +
-                    self.w_food * F +
-                    self.w_enemy * Rv
-                )
-                self.X[i] += self.V[i]
-            else:
-                # random walk
-                self.X[i] += np.random.randn(tasks_num) * (self.X[i] - self.worst_pos)
-            self.X[i] = np.clip(self.X[i], 0, machines_num - 1)
-
-        fitness = np.array([Common.vector_fitness(x, self.etc, self.machines, Common.scheduling_mode)[0] for x in self.X])
-        cb = np.argmin(fitness)
-        cw = np.argmax(fitness)
-        if fitness[cb] < prev_best_val:
-            self.best_pos = self.X[cb].copy()
-        if fitness[cw] > prev_worst_val:
-            self.worst_pos = self.X[cw].copy()
-
-
-if __name__ == "__main__":
-    alg = DragonflyMethod(iterations=100, population_size=30, w_inertia=0.9, w_separation=0.1, w_alignment=0.1, w_cohesion=0.1, w_food=2.0, w_enemy=1.0, neighbour_radius_factor=0.5, show_chart=True)
-    alg.run()
+            self._evaluate_population_update_best()
+            self._epoch += 1
